6d9a7e22b507e03cc1680a042d3ad183740ecdd7
jose.fajardo <fajardo_jf@hotmail.com>
2018-09-19 06:01:01 +1000

    JF.UI : started work on sqlite and nodegraph

---
+++ src/ImGui.NET.SampleProgram/NodeGraph.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImGui.NET.SampleProgram
+{
+  public static class NodeGraph
+  {
+
+    public static void ShowExampleAppCustomNodeGraph(Boolean opened)
+    {
+      List<Node> nodes = new List<Node>() ;
+      List<NodeLink> links;
+      bool inited = false;
+      System.Numerics.Vector2 scrolling = new System.Numerics.Vector2(0.0f, 0.0f);
+      bool show_grid = true;
+      int node_selected = -1;
+
+      if (!inited) {
+        //nodes.Add(Node(){ })
+      }
+
+    }
+
+  }
+
+
+
+
+
+
+  /*
+  struct Node
+  {
+    int ID;
+    char Name[32];
+    ImVec2 Pos, Size;
+    float Value;
+    ImVec4 Color;
+    int InputsCount, OutputsCount;
+
+    Node(int id, const char* name, const ImVec2& pos, float value, const ImVec4& color, int inputs_count, int outputs_count) { ID = id; strncpy(Name, name, 31); Name[31] = 0; Pos = pos; Value = value; Color = color; InputsCount = inputs_count; OutputsCount = outputs_count; }
+    ImVec2 GetInputSlotPos(int slot_no) const { return ImVec2(Pos.x, Pos.y + Size.y* ((float) slot_no + 1) / ((float) InputsCount + 1)); }
+    ImVec2 GetOutputSlotPos(int slot_no) const { return ImVec2(Pos.x + Size.x, Pos.y + Size.y* ((float) slot_no + 1) / ((float) OutputsCount + 1)); }
+  };
+  */
+
+  public class Node
+  {
+    int ID;
+    char[] Name;
+    System.Numerics.Vector2 Pos;
+    System.Numerics.Vector2 Size;
+    float Value;
+    System.Numerics.Vector4 Color;
+    int InputsCount;
+    int OutputsCount;
+
+    Node() {
+    }
+  }
+
+
+
+
+
+
+  /*
+  struct NodeLink
+  {
+    int InputIdx, InputSlot, OutputIdx, OutputSlot;
+    NodeLink(int input_idx, int input_slot, int output_idx, int output_slot) { InputIdx = input_idx; InputSlot = input_slot; OutputIdx = output_idx; OutputSlot = output_slot; }
+
+  };
+  */
+
+  public class NodeLink {
+    int InputIdx;
+    int InputSlot;
+    int OutputIdx;
+    int OutputSlot;
+  }
+
+}
+
---
+++ src/ImGui.NET.SampleProgram/SQLite/SQLiteDataEntity.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace X.CoreLib.Shared.Framework.Services.DataEntity
+{
+  public class BaseEntity
+  {
+    public Guid UniqueId { get; set; }
+    public int _internalRowId;
+  }
+
+  public class SQLiteDataEntity<T> where T : BaseEntity, new()
+  {
+    public string _defaultCreator = "admin";
+    private bool _isReadOnly = false;
+
+    string _tableName;
+    TableDatabase _table;
+
+    public SQLiteDataEntity() { SQLiteDataEntityImpl(true); }
+    public SQLiteDataEntity(bool initDb, bool isReadOnly) { SQLiteDataEntityImpl(initDb, isReadOnly); }
+    private void SQLiteDataEntityImpl(bool initDb = true, bool isReadOnly = false)
+    {
+      _tableName = typeof(T).Name;
+      _isReadOnly = isReadOnly;
+      if (initDb) { InitEntityDatabase(); }
+      else { _table = AppDatabase.Current.Tables[_tableName]; }
+    }
+
+
+    public static SQLiteDataEntity<T> Create()
+    {
+      return new SQLiteDataEntity<T>();
+    }
+    public static SQLiteDataEntity<T> CreateReadOnly()
+    {
+      return new SQLiteDataEntity<T>(false, true);
+    }
+
+    //todo: optimization on columns to determine if they changed and thus do a DeleteAllColumns and rebuild
+    private void InitEntityDatabase()
+    {
+      AppDatabase.Current.AddTable(_tableName, _defaultCreator);
+      _table = AppDatabase.Current.Tables[_tableName];
+
+      _table.DeleteAllColumns();  //note: we delete all columns and rebuild them from the current class to ensure new columns exist
+
+      var props = typeof(T).GetTypeInfo().DeclaredProperties;
+      foreach (var prop in props)
+      {
+        _table.AddColumn(prop.Name, _defaultCreator);
+      }
+
+      _table.AddColumn("UniqueId", _defaultCreator);
+    }
+
+    public int Save(T instance)
+    {
+      if (_isReadOnly) return 0;
+
+      if (instance.UniqueId == Guid.Empty) instance.UniqueId = Guid.NewGuid();
+
+      if (instance._internalRowId > 0)
+      {
+        //update
+        var udo = _table.GetRowDataAsJson(instance._internalRowId);
+
+        var props = typeof(T).GetTypeInfo().DeclaredProperties;
+        foreach (var prop in props)
+        {
+          udo[prop.Name] = prop.GetValue(instance, null).ToString();
+        }
+        udo["UniqueId"] = instance.UniqueId.ToString();
+        _table.UpdateRowData(instance._internalRowId, udo);
+      }
+      else
+      {
+        //add
+
+        var udo = _table.GetEmptyRowAsJson();
+
+        var props = typeof(T).GetTypeInfo().DeclaredProperties;
+        foreach (var prop in props)
+        {
+          udo[prop.Name] = prop.GetValue(instance, null).ToString();
+        }
+
+        udo["UniqueId"] = instance.UniqueId.ToString();
+
+        instance._internalRowId = _table.AddRow(udo, _defaultCreator);
+      }
+
+      return instance._internalRowId;
+    }
+    public T Retrieve(int id)
+    {
+      T obj = new T();
+
+      // clear();
+
+      try
+      {
+        var udo = _table.GetRowDataAsJson(id);
+
+        obj._internalRowId = id;
+
+        var props = typeof(T).GetTypeInfo().DeclaredProperties;
+        foreach (var prop in props)
+        {
+          switch (prop.PropertyType.Name)
+          {
+            case "String": prop.SetValue(obj, (string)udo[prop.Name]); break;
+            case "Guid": prop.SetValue(obj, Guid.Parse((string)udo[prop.Name])); break;
+            case "DateTime": prop.SetValue(obj, DateTime.Parse((string)udo[prop.Name])); break;
+            case "Long": prop.SetValue(obj, long.Parse((string)udo[prop.Name])); break;
+            case "Int": prop.SetValue(obj, int.Parse((string)udo[prop.Name])); break;
+            case "Double": prop.SetValue(obj, Double.Parse((string)udo[prop.Name])); break;
+          }
+        }
+        obj.UniqueId = Guid.Parse((string)udo["UniqueId"]);
+
+        return obj;
+      }
+      catch (Exception ex)
+      {
+        obj._internalRowId = 0;
+        obj.UniqueId = Guid.Empty;
+      }
+
+      return obj;
+    }
+
+    public List<TableRow> FindResult;
+    public int Find(string whereQuery)
+    {
+      FindResult = _table.Find(whereQuery);
+      return FindResult.Count;
+    }
+    public int FindAll()
+    {
+      FindResult = _table.GetRows();
+      return FindResult.Count;
+    }
+    public void Delete(T instance)
+    {
+      if (_isReadOnly) return;
+
+      if (instance._internalRowId > 0)
+      {
+        _table.DeleteRow(instance._internalRowId);
+      }
+
+      clear(instance);
+    }
+    public void Delete(int id)
+    {
+      if (_isReadOnly) return;
+      _table.DeleteRow(id);
+    }
+    public void DeleteAll()
+    {
+      _table.DeleteAllRows();
+    }
+
+    private void clear(T instance)
+    {
+      var props = typeof(T).GetTypeInfo().DeclaredProperties;
+      foreach (var prop in props)
+      {
+        prop.SetValue(instance, null);
+      }
+
+      instance._internalRowId = 0;
+      instance.UniqueId = Guid.Empty;
+    }
+  }
+}
